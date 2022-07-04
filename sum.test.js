5f2d32035af2d91ac354b92626b1a4916e10ca4d
gautam-msys <gkumar@msystechnologies.com>
2022-07-04 12:59:39 +0530

    Added test cases for multiplication function

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 const sum = require("./sum");
+const sum = require("./multiply");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("multiplies 1 * 2 to equal 2", () => {
+    expect(multiply(1, 2)).toBe(2);
+  });
